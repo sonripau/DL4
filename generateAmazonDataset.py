0625105e1b6cc43ce77424864c83f0acc8cb2f2c
sonripau <113700350+sonripau@users.noreply.github.com>
2025-04-22 15:44:29 +0200

    Add files via upload

---
+++ generateAmazonDataset.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+#reads a file. Each line has the format: label text
+#Returns a list with the text and a list with the labels
+def readData(fname):
+
+    with open(fname, 'r', encoding="utf-8") as f:
+        fileData = f.read()
+  
+    lines = fileData.split("\n")
+    textData = list()
+    textLabel = list()
+    lineLength = np.zeros(len(lines))
+    
+    for i, aLine in enumerate(lines):     
+        if not aLine:
+            break  
+        label = aLine.split(" ")[0]
+        lineLength[i] = len(aLine.split(" "))
+        if(label == "__label__1"):
+            textLabel.append(0)
+            textData.append(aLine.removeprefix("__label__1 "))
+
+        elif(label == "__label__2"):
+            textLabel.append(1)
+            textData.append(aLine.removeprefix("__label__2 "))
+
+        else:
+            print("\nError in readData: ", i, aLine)
+            exit()
+    
+    f.close()
+    return textData, textLabel, int(np.average(lineLength)+2*np.std(lineLength))
+
+
+# In[3]:
+
+
+def transformData(x_train, y_train, x_test, y_test, maxFeatures, seqLength):
+    #transforms text input to int input based on the vocabulary
+    #max_tokens = maxFeatures is the size of the vocabulary
+    #output_sequence_length =  seqLength is the maximum length of the transformed text. Adds 0 is text length is shorter
+    precLayer = layers.TextVectorization(max_tokens = maxFeatures, 
+    standardize =  'lower_and_strip_punctuation', split = 'whitespace', output_mode = 'int', 
+    output_sequence_length =  seqLength)
+    precLayer.adapt(x_train)
+    #print(precLayer.get_vocabulary())
+    x_train_int = precLayer(x_train)
+    y_train = tf.convert_to_tensor(y_train)
+    #print(x_train_int)
+    #print(y_train)
+    x_test_int= precLayer(x_test)
+    y_test = tf.convert_to_tensor(y_test)
+    #print(x_test_int)
+    #print(y_test)
+
+    return x_train_int, y_train, x_test_int, y_test
+
+
+# In[ ]:
+
+
+#x_train, y_train, seqLength = readData("amazon/train_small.txt")
+#x_test, y_test, tmp = readData("amazon/test_small.txt")
+
+#maxFeatures is a hyperparameter
+
+#x_train_int, y_train, x_test_int, y_test = transformData(x_train, y_train, x_test, y_test, maxFeatures, seqLength)
+
